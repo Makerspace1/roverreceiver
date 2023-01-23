59e60efb3690f9847f2a4993efdf57c750546ca4
Makerspace1 <123253544+Makerspace1@users.noreply.github.com>
2023-01-23 14:22:08 +0100

    Translations to Norks. Fixed bug where motors keep spinning on fault

--- main.ts
+++ main.ts
@@ -35,23 +35,29 @@ function CommandMotor (params: any[]) {
     if (!(ValidateParams(3, params))) {
         return false
     }
-    commandMotorDuration = parseFloat(params.shift())
-    commandMotorSpeed = parseFloat(params.shift())
+    commandMotorDuration = parseFloat("")
+    commandMotorSpeed = parseFloat("")
     while (params.length > 0) {
         commandMotorName = params.shift()
         commandMotorDirection = params.shift()
-        if (commandMotorDirection != 0 && commandMotorDirection != 1) {
+        let commandMotorDirectionEnum = motor.Dir.CW
+if (commandMotorDirection == 0) {
+            commandMotorDirectionEnum = motor.Dir.CW
+        } else if (commandMotorDirection == 1) {
+            commandMotorDirectionEnum = motor.Dir.CCW
+        } else {
             SendError("Unknown motor direction. Expected 0 or 1")
+            motor.motorStopAll()
             return false
         }
-        if (commandMotorName == "LV") {
-            motor.MotorRun(motor.Motors.M1, commandMotorDirection, commandMotorSpeed)
-        } else if (commandMotorName == "RV") {
-            motor.MotorRun(motor.Motors.M2, commandMotorDirection, commandMotorSpeed)
-        } else if (commandMotorName == "LA") {
-            motor.MotorRun(motor.Motors.M3, commandMotorDirection, commandMotorSpeed)
-        } else if (commandMotorName == "RA") {
-            motor.MotorRun(motor.Motors.M4, commandMotorDirection, commandMotorSpeed)
+        if (commandMotorName == "VF") {
+            motor.MotorRun(motor.Motors.M1, commandMotorDirectionEnum, commandMotorSpeed)
+        } else if (commandMotorName == "HF") {
+            motor.MotorRun(motor.Motors.M2, commandMotorDirectionEnum, commandMotorSpeed)
+        } else if (commandMotorName == "VB") {
+            motor.MotorRun(motor.Motors.M3, commandMotorDirectionEnum, commandMotorSpeed)
+        } else if (commandMotorName == "HB") {
+            motor.MotorRun(motor.Motors.M4, commandMotorDirectionEnum, commandMotorSpeed)
         } else {
             SendError("Unknown motor")
             motor.motorStopAll()
@@ -66,6 +72,7 @@ function RunCommandQueue () {
     while (commandQueue.length > 0) {
         // basic.showNumber(commandQueue.length)
         if (!(ParseCommand(commandQueue.shift()))) {
+            motor.motorStopAll()
             SendError("Error while executing queue. Clearing queue")
             ClearCommandQueue()
             basic.showIcon(IconNames.Sad)
@@ -103,14 +110,14 @@ function CommandServo (params: any[]) {
     if (!(ValidateParams(2, params))) {
         return false
     }
-    if (params[0] == "LV") {
-        motor.servo(motor.Servos.S1, parseFloat(params[1]))
-    } else if (params[0] == "RV") {
-        motor.servo(motor.Servos.S2, parseFloat(params[1]))
-    } else if (params[0] == "LA") {
-        motor.servo(motor.Servos.S3, parseFloat(params[1]))
-    } else if (params[0] == "RA") {
-        motor.servo(motor.Servos.S4, parseFloat(params[1]))
+    if (params[0] == "VF") {
+        motor.servo(motor.Servos.S1, parseFloat(""))
+    } else if (params[0] == "HF") {
+        motor.servo(motor.Servos.S2, parseFloat(""))
+    } else if (params[0] == "VB") {
+        motor.servo(motor.Servos.S3, parseFloat(""))
+    } else if (params[0] == "HB") {
+        motor.servo(motor.Servos.S4, parseFloat(""))
     } else {
         SendError("Unknown servo")
         return false
@@ -141,14 +148,14 @@ radioRecBuffer = radioRecBuffer.substr(radioRecBuffer.indexOf("\n") + 1, 0)
 })
 let mainCommand = ""
 let commandSegmented: string[] = []
+let commandMotorDirection = 0
 let commandMotorName = ""
 let commandMotorDuration = 0
 let commandQueue: string[] = []
 let maxMessageLength = 0
-let commandMotorSpeed = 0
-let commandMotorDirection = 0
-let radioRecBuffer = ""
 let receivedCommand = ""
+let radioRecBuffer = ""
+let commandMotorSpeed = 0
 maxMessageLength = 18
 radio.setGroup(1)
 basic.showString("Init")
