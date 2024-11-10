74cad88bad32acfcf3d679ad79f4d42530df0751
Makerspace1 <123253544+Makerspace1@users.noreply.github.com>
2024-11-10 11:16:50 +0100

    Changed radio channnel to 83, radioeffekt  7

--- main.ts
+++ main.ts
@@ -170,7 +170,8 @@ let radioRecBuffer = ""
 let receivedCommand = ""
 maxMessageLength = 18
 radio.setGroup(255)
-radio.setFrequencyBand(50)
+radio.setFrequencyBand(83)
+radio.setTransmitPower(7)
 basic.showString("Init")
 rovershield.servo(rovershield.Servos.S1, 90)
 rovershield.servo(rovershield.Servos.S2, 90)
